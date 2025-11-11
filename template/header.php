21c7d783cc0a310baab9d56ee8eed542e7f85f29
Herianto <80012416+harryandjune@users.noreply.github.com>
2025-11-11 09:31:37 +0800

    Add files via upload

---
+++ template/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link id="favicon" rel="icon" href="img/calculator.svg" rel="shortcut icon" type="image/png" type="image/x-icon">
+
+
+    <title>Zakatku</title>
+</head>
+
+<body>
+
+    <header class="container col-12 bg-primary text-white rounded-bottom">
+        <!-- <div class="row"> -->
+        <div class="col-12 py-2 text-center">
+            <h1 class="display-5">Zakatku</h1>
+            <p class="lead text-white rounded">Aplikasi perhitungan zakat dengan update nishob setiap hari dari www.harga-emas.com !</p>
+            <!-- </div> -->
+        </div>
+
+    </header>
